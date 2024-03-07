ebfab1a2c2dea5b3d0f69b794691dd4d28633f4a
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-07 17:32:25 +0000

    Update game.js

--- game.js
+++ game.js
@@ -13,3 +13,31 @@ function displayVisitedPages() {
 function goToPage(page) {
   window.location.href = page;
 }
+
+function performSearch() {
+    var searchQuery = document.getElementById('search').value.toLowerCase();
+    var fileLinks = getServerFileLinks();
+    var searchResultsDiv = document.getElementById('searchResults');
+    searchResultsDiv.innerHTML = "";
+
+    for (var i = 0; i < fileLinks.length; i++) {
+        var fileName = fileLinks[i].name.toLowerCase();
+
+        if (fileName.includes(searchQuery)) {
+            var resultItem = document.createElement('a');
+            resultItem.textContent = fileLinks[i].name;
+            resultItem.href = fileLinks[i].link;
+            searchResultsDiv.appendChild(resultItem);
+        }
+    }
+
+    var buttons = document.getElementsByClassName('game_button');
+    for (var j = 0; j < buttons.length; j++) {
+        var buttonName = buttons[j].textContent.toLowerCase();
+        if (!buttonName.includes(searchQuery)) {
+            buttons[j].style.display = 'none';
+        } else {
+            buttons[j].style.display = 'block';
+        }
+    }
+}
