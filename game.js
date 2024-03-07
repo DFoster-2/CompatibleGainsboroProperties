edead86b8011f9a65f8bc964a35b0a30221d9840
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-07 18:40:01 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,15 @@
 var visitedPages = JSON.parse(localStorage.getItem('visitedPages')) || [];
 var visitedPagesContainer = document.getElementById('visitedPages');
-visitedPagesContainer.innerHTML = '';
+visitedPagesContainer.innerHTML = '<h2>Visited Pages:</h2>';
 if (visitedPages.length === 0) {
-  visitedPagesContainer.innerHTML += '<p>You have not played any games on this computer</p> ';
+  visitedPagesContainer.innerHTML += '<p>No pages visited yet.</p>';
 } else {
   visitedPages.forEach(function (page) {
-    visitedPagesContainer.innerHTML += `<button onclick="goToPage('${page}')">${page}</button>`;
+  var cleanFileName = getCleanFileName(page);
+  visitedPagesContainer.innerHTML += `<button onclick="goToPage('${page}')">${cleanFileName}</button>`;
   });
 }
-
+ 
 function goToPage(page) {
   window.location.href = page;
 }
