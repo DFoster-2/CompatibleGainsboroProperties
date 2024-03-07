0a1abe827bcf5db6f6680902ade08c5db1f5619a
DFoster-2 <138312355+DFoster-2@users.noreply.github.com>
2024-03-07 18:18:13 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,14 @@
-function displayVisitedPages() {
-  var visitedPages = JSON.parse(localStorage.getItem('visitedPages')) || [];
-  var visitedPagesContainer = document.getElementById('visitedPages');
-  visitedPagesContainer.innerHTML = '<h2>Visited Pages:</h2>';
-  if (visitedPages.length === 0) {
-    visitedPagesContainer.innerHTML += '<p>No pages visited yet.</p>';
-  } else {
-    visitedPages.forEach(function (page) {
-      visitedPagesContainer.innerHTML += `<button onclick="goToPage('${page}')">${page}</button>`;
-    });
-  }
+var visitedPages = JSON.parse(localStorage.getItem('visitedPages')) || [];
+var visitedPagesContainer = document.getElementById('visitedPages');
+visitedPagesContainer.innerHTML = '<h2>Visited Pages:</h2>';
+if (visitedPages.length === 0) {
+  visitedPagesContainer.innerHTML += '<p>No pages visited yet.</p>';
+} else {
+  visitedPages.forEach(function (page) {
+    visitedPagesContainer.innerHTML += `<button onclick="goToPage('${page}')">${page}</button>`;
+  });
 }
+
 function goToPage(page) {
   window.location.href = page;
 }
